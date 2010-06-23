37f2d0ae02dea3c39843150d4b5729520b9a847c
Brett Haydon <brett@haydon.id.au>
2010-06-23 14:02:50 +1000

    bump the version to 0.2 pre-alpha

--- woven/__init__.py
+++ woven/__init__.py
@@ -4,7 +4,7 @@ All :mod:`woven.main` functions can be imported directly from here instead
 """
 from woven.main import setup_environ, setupnode
 
-VERSION = (0, 1, 1, 'final', 1)
+VERSION = (0, 2, 0, 'alpha', 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
