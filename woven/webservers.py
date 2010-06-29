8f7138f66df4cb29ac9a25269db4d805d13e7037
Brett Haydon <brett@haydon.id.au>
2010-06-29 17:35:53 +1000

    added deploy_webservers, and default webserver templates

--- woven/webservers.py
+++ woven/webservers.py
@@ -17,21 +17,46 @@ from fabric.contrib.console import confirm
 from fabric.version import get_version
 
 from woven.utils import root_domain, server_state, set_server_state
-from woven.utils import project_fullname, project_name, active_version
+from woven.utils import project_fullname, project_name, project_version, active_version
 from woven.utils import upload_template
 
 from woven.project import Project
 
+def _ls_sites(path):
+    """
+    List only sites in the env.DOMAINS to ensure we co-exist with other projects
+    
+    """
+    
+    sites = run('ls %s'% path).split('\n')
+    doms = env.DOMAINS
+    dom_sites = []
+    for s in sites:
+        ds = s.split('-')[0]
+        ds = ds.replace('_','.')
+        if ds in doms and s not in dom_sites:
+            dom_sites.append(s)
+    return dom_sites
+
+def sites_enabled():
+    """
+    Get a list of apache sites enabled
+    """
+    path = "/etc/apache2/sites-enabled/"
+    if exists(path, use_sudo=True):
+        sites_enabled = _ls_sites(path)
+    return sites_enabled
+
 class WSGI(Project):
     """
-    Deploy a modwsgi file per domain
+    Deploy a modwsgi file per domain and base class for webserver classes
     """
     
     state = 'deployed_wsgi_'
     def __init__(self,version=''):
         super(WSGI,self).__init__(version)
-        self.setting =''
-        self.dest_path_postfix = ''
+        self.template = 'django-wsgi-template.txt'
+        
     
     def deploy(self, patch=False,domain=''):
         deploy_root = self.deploy_root + domain
@@ -45,29 +70,29 @@ class WSGI(Project):
                        "u_domain":domain,
                        "root_domain":env.root_domain,
                        }
-            wsgi_template = 'django-wsgi-template.txt'
             wsgi_exists = exists(filename)
             if wsgi_exists and not patch: 
-                print env.host,"WSGI already exists on the host %s. Skipping..."% filename
+                print env.host,"%s already exists on the host %s. Skipping..."% (self.deploy_type,filename)
                 return False
             elif not wsgi_exists and patch:
-                print env.host,"Error: Cannot patch %s wsgi. This version does not exist"% project_fullname
+                print env.host,"Error: Cannot patch %s %s. This version does not exist"% (project_fullname, self.deploy_type)
                 return False
             current_version = active_version()
             if current_version == env.project_fullname and not patch:
-                print env.host,"Warning: Cannot deploy wsgi, version %s already active. Increase the version and re-deploy. Skipping.."% env.project_fullname
+                print env.host,"Warning: Cannot deploy %s, version %s already active. Increase the version and re-deploy. Skipping.."% (self.deploy_type,env.project_fullname)
                 return False
             else: #not exists
-                upload_template('woven/'+wsgi_template,
+                upload_template('woven/'+self.template,
                                     filename,
                                     context,
                                 )
                 
                 #finally set the ownership/permissions
-                #We'll use the group to allow www-data execute 
-                sudo("chown %s:www-data %s"% (env.user,filename))
-                run("chmod ug+xr %s"% filename)
-        set_server_state('deployed_wsgi_'+env.project_fullname)        
+                #We'll use the group to allow www-data execute
+                if self.deploy_type == 'wsgi':
+                    sudo("chown %s:www-data %s"% (env.user,filename))
+                    run("chmod ug+xr %s"% filename)
+        set_server_state(self.state+env.project_fullname)        
         
 
 def deploy_wsgi(version='',patch=False):
@@ -78,5 +103,115 @@ def deploy_wsgi(version='',patch=False):
     for domain in env.DOMAINS:
         w = WSGI(version)
         w.deploy(patch,domain)
+
+class ApacheWebserver(Project):
+    """
+    Deploy Apache webserver configuration
+    """
+    state = 'deployed_apache_webserver_'
+    def __init__(self,version=''):
+        super(ApacheWebserver,self).__init__(version)
+        self.template = 'django-apache-template.txt'
+        self.deploy_root = "/etc/apache2/sites-available/"
+        self.enabled_path = "/etc/apache2/sites-enabled/"
+        self.version = project_version(version)
+        if not 'http:' in env.MEDIA_URL: self.media_url = env.MEDIA_URL
+        else: self.media_url = ''
+        if not 'http:' in env.STATIC_URL: self.static_url = env.STATIC_URL
+        else: self.static_url = ''            
+
+    
+    def deploy(self,domain,patch=False):
+        with cd(self.deploy_root):
+            log_dir = env.deployment_root+'log'
+            if not exists(log_dir):
+                run("mkdir -p %s"% log_dir)
+                sudo("chown -R www-data:sudo %s" % log_dir)
+                sudo("chmod -R ug+w %s"% log_dir)
+            #doc_root = os.path.join(project_env,'public',domain)
+            #if not exists(doc_root):
+            #    run("mkdir -p %s" % doc_root)
+            u_domain = domain.replace('.','_')
+            if patch:
+                filename = u_domain+'-'+patch + '.conf'
+            else:
+                filename = u_domain + '-'+self.version+'.conf'
+            context = {"project_name": env.project_name,
+                        "u_domain":u_domain,
+                        "domain":domain,
+                        "root_domain":env.root_domain,
+                        "user":env.user,
+                        "host_ip":env.host,
+                        "media_url":self.media_url,
+                        "static_url":self.static_url,
+                        }
+                
+            conf_exists = exists(os.path.join(self.enabled_path,filename), use_sudo=True)
+            state = server_state(self.state+env.project_fullname)
+
+            if not conf_exists and patch:
+                if env.verbosity:
+                    print env.host,"Cannot patch %s conf %s. This version does not exist on %s. Skipping"% (self.deploy_type, filename, env.host)
+                return False
+            elif state and not patch: #active version
+                print "%s conf %s already exists on the host %s. Skipping"% (self.deploy_type, filename, env.host)
+                return False
+            else:
+                enabled_sites = _ls_sites(self.enabled_path)
+                if not patch:
+                    for site in enabled_sites:
+                        if env.verbosity:
+                            print env.host,'Disabling', site, filename
+                        sudo("rm %s%s"% (self.enabled_path,site))
+                print 'Uploading template'
+                upload_template('woven/'+self.template,
+                                filename,
+                                context,
+                                use_sudo=True)
+                if not patch:
+                    #enable this site
+                    sudo("chmod 644 %s" % filename)
+                    if not exists(self.enabled_path+ filename):
+                        sudo("ln -s %s%s %s%s"% (self.deploy_root,filename,self.enabled_path,filename))
+        set_server_state(self.state + self.fullname)
+
+class NginxWebserver(ApacheWebserver):
+    state = 'deployed_nginx_webserver_'
+    def __init__(self,version=''):
+        super(NginxWebserver,self).__init__(version)
+        self.template = 'nginx-template.txt'
+        self.deploy_root = "/etc/nginx/sites-available/"
+        self.enabled_path = "/etc/nginx/sites-enabled/"
         
+def deploy_webservers(version='',patch=False):
+    """ Deploy an apache & nginx conf to the host """
+    if not env.DOMAINS: env.DOMAINS = [root_domain()]
+    if exists('/etc/apache2/sites-enabled/') and exists('/etc/nginx/sites-enabled'):
+        #TODO - INSERT MAINTENANCE PAGE IN HERE AND RELOAD NGINX
+        sudo("/etc/init.d/nginx stop")
+        for d in env.DOMAINS:
+            #TODO - distinguish between a warning and a error on apache
+            with settings(warn_only=True):
+                a = sudo("apache2ctl stop")
+                if a.failed and env.verbosity:
+                    print env.host, a            
+            a = ApacheWebserver(version)
+            a.deploy(d,patch)
+            
+
+            a = sudo("apache2ctl start")
+            if a.failed and env.verbosity:
+                print env.host, a
+            
+            n = NginxWebserver(version)
+            n.deploy(d,patch)
+        
+        sudo("/etc/init.d/nginx start")
+        set_server_state('deployed_webservers_' + project_fullname())
+        return True
+
+    else:
+        print """WARNING: Apache or Nginx not installed"""
+        return False
+    return False        
     
