8cc99a067512e81a7149d809615841a2e7d57eea
Brett Haydon <brett@haydon.id.au>
2010-07-18 16:10:02 +1000

    added a deployment module for deploying generic files and directories. Has much more flexibility than simple rsync wrapper and deploy_files function returns a rich State object with data attached to it including stderr, and a results object with a list of files and directories created. The module also includes a run_once_per_version decorator which will store results and success or failure as state on the host. This should be much more flexible than trying to have deployment classes.

---
+++ woven/deployment.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+from functools import wraps
+from glob import glob
+import os, shutil, sys, tempfile
+
+from django.template.loader import render_to_string
+
+from fabric.state import env
+from fabric.operations import _AttributeString, run, sudo
+from fabric.context_managers import cd
+from fabric.contrib.files import exists
+from fabric.contrib.project import rsync_project
+#Required for a bug in 0.9
+from fabric.version import get_version
+
+from woven.utils import project_name, mkdirs
+from woven.utils import server_state, set_server_state, State
+
+def run_once_per_version(func):
+    """
+    Decorator preventing wrapped function from running more than
+    once per host and env.project_version not just interpreter session.
+    
+    If env.project_version is not set then it will effectively be once per host.
+    Using env.patch = True will allow the function to be run
+    
+    Stores the result of a function as server state for rollback
+    
+    Returns an attribute string
+    """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if not hasattr(env,'patch'): env.patch = False
+        state = server_state(func.__name__)
+        if not env.patch and state:
+            state.failed = False       
+            verbose = " ".join([env.host,state,"completed. Skipping..."])
+        elif env.patch and not state:
+            verbose = " ".join([env.host,state,"not previously completed. Skipping..."])
+        else:
+            state = func(*args, **kwargs)
+            verbose =''
+            #if the returning function is a state object with an object attr we will store it as json in the file 
+            if state: set_server_state(func.__name__,object=getattr(state,'object',None))
+        if env.verbosity and verbose: print verbose
+        return state            
+          
+    return decorated
+
+def _get_local_files(local_dir, pattern=''):
+    """
+    Returns a dictionary with directories as keys, and filenames as values
+    for filenames matching the glob ``pattern`` under the ``local_dir``
+    ``pattern can contain the Boolean OR | to evaluated multiple patterns into
+    a combined set. 
+    """
+    local_files = {}
+    
+    if pattern:
+        cwd = os.getcwd()
+        os.chdir(local_dir)
+        patterns = pattern.split('|')
+        local_list = set([])
+        for p in patterns: local_list = local_list | set(glob(p))
+        for path in local_list:
+            dir, file = os.path.split(path)
+            if os.path.isfile(path):
+                local_files[dir] = local_files.get(dir,[])+[file]
+            elif os.path.isdir(path):
+                local_files[file] = local_files.get(dir,[])
+        os.chdir(cwd)
+    return local_files
+
+def _stage_local_files(local_dir, local_files={}, context={}):
+    """
+    Either ``local_files`` and/or ``context`` should be supplied.
+    
+    Will stage a ``local_files`` dictionary of path:filename pairs where path
+    is relative to ``local_dir`` into a local tmp staging directory.
+    
+    Supply a ``context`` to render templates under ``local_dir`` or restricted to
+    ``local_files`` and save into the local tmp staging directory.
+    
+    Returns a path to the temporary local staging directory
+
+    """
+    staging_dir = os.path.join(tempfile.mkdtemp(),os.path.basename(local_dir))
+    os.mkdir(staging_dir)
+    for root, dirs, files in os.walk(local_dir):
+        relative_tree = root.replace(local_dir,'')
+        if relative_tree:
+            relative_tree = relative_tree[1:]
+        if local_files:
+            files = local_files.get(relative_tree,[])
+        for file in files:
+            if relative_tree:
+                filepath = os.path.join(relative_tree,file)
+                if not os.path.exists(os.path.join(staging_dir,relative_tree)):
+                    os.mkdir(os.path.join(staging_dir,relative_tree))
+            else: filepath = file
+            if context: #render templates
+                text = render_to_string(os.path.join(root,file),context)
+                output = open(os.path.join(staging_dir,filepath), "w+b")
+                output.write(text)
+                output.close()
+            else:
+                shutil.copy2(os.path.join(root,file),os.path.join(staging_dir,filepath))
+    return staging_dir
+
+def deploy_files(local_dir, remote_dir, pattern = '', context={}, chown='', chmod='', rsync_exclude=['*.pyc','.*'], use_sudo=False):
+    """
+    Generic deploy function for cases where one or more files are being deployed to a host.
+    Wraps around ``rsync_project`` and stages files locally and/or remotely
+    for network efficiency.
+    
+    ``local_dir`` is the directory that will be deployed.
+   
+    ``remote_dir`` is the directory the files will be deployed to.
+    Directories will be created if necessary.
+    
+    Note: Unlike other ways of deploying files, all files under local_dir will be
+    deployed into remote_dir. This is the equivalent to cp -R local_dir/* remote_dir.
+
+    ``pattern`` finds all the pathnames matching a specified glob pattern relative
+    to the local_dir according to the rules used by the Unix shell.
+    ``pattern`` enhances the basic functionality by allowing the python | to include
+    multiple patterns. eg '*.txt|Django*'
+    
+    By including a ``context``, files will be processed as templates. No attempt is
+    made to determine that the files that are being processed are actually templates.
+    Templates are always rendered and uploaded even if the template hasn't changed.
+    
+    ``chown`` chown args and options to run recursively (-R)
+    ``chmod`` chmod args and options to run recursively (-R)
+    
+    ``rsync_exclude`` as per ``rsync_project``
+    
+    Returns a list of directories and files created on the host.
+    
+    """
+    #normalise paths
+    if local_dir[-1] == os.sep: local_dir = local_dir[:-1]
+    if remote_dir[-1] == '/': remote_dir = remote_dir[:-1]
+    created_list = []
+    staging_dir = local_dir
+    
+    #resolve pattern into a dir:filename dict
+    local_files = _get_local_files(local_dir,pattern)
+    #If we are only copying specific files or rendering templates we need to stage locally
+    if local_files or context: staging_dir = _stage_local_files(local_dir, local_files, context)
+    remote_staging_dir = '/home/%s/.staging'% env.user
+    if not exists(remote_staging_dir):
+        run(' '.join(['mkdir -pv',remote_staging_dir])).split('\n')
+        created_list = [remote_staging_dir]
+    
+    #bug in fabric 0.9 on rsync on alternate port fixed in 1.0
+    fab_vers = int(get_version(form='short')[0])
+    if fab_vers < 1: extra_opts = '--rsh="ssh -p%s"'% env.port
+    else: extra_opts = None
+    
+    #upload into remote staging
+    rsync_project(local_dir=staging_dir,remote_dir=remote_staging_dir,extra_opts=extra_opts,exclude=rsync_exclude,delete=True)
+
+    #create the final destination
+    created_dir_list = mkdirs(remote_dir, use_sudo)
+
+    func = use_sudo and sudo or run
+    #cp recursively -R from the staging to the destination and keep a list
+    remote_base_path = '/'.join([remote_staging_dir,os.path.basename(local_dir),'*'])
+    copy_file_list = func(' '.join(['cp -Ruvp',remote_base_path,remote_dir])).split('\n')
+    if copy_file_list[0]: created_list += [file.split(' ')[2][1:-1] for file in copy_file_list if file]
+    
+    #chmod
+    #chown
+    
+    #cleanup any tmp staging dir
+    if staging_dir <> local_dir:
+        shutil.rmtree(staging_dir,ignore_errors=True)
+    
+    return created_list
