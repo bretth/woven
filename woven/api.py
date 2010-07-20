f8a3994e75970354b1e667436e9724b53df35709
Brett Haydon <brett@haydon.id.au>
2010-07-20 20:50:06 +1000

    changed main to api to expose the woven functions

---
+++ woven/api.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Exposes the full public woven api
+"""
+from woven.deployment import deploy_files, mkdirs
+from woven.deployment import upload_template, run_once_per_host_version
+
+from woven.ubuntu import add_user, apt_get_install, apt_get_purge
+from woven.ubuntu import install_packages, upgrade_ubuntu, setup_ufw, disable_root
+from woven.ubuntu import uncomment_sources, restrict_ssh, upload_ssh_key
+from woven.ubuntu import change_ssh_port, set_timezone, ubuntu_version
+
+from woven.virtualenv import activate, active_version
+from woven.virtualenv import mkvirtualenv, rmvirtualenv, pip_install_requirements
+
+from woven.project import deploy_static, deploy_public, deploy_project, deploy_db, deploy_templates
+from woven.project import project_name, project_fullname, project_version
+
+from woven.webservers import deploy_wsgi, deploy_webservers, start_webservices, stop_webservices
+from woven.environment import set_env, set_project_env, patch_project, server_state, set_server_state, verbosity
+
+
+def deploy():
+    """
+    deploy a versioned project on the host
+    """
+    deploy_funcs = [deploy_project,deploy_templates, deploy_static, deploy_public,  deploy_webservers, deploy_wsgi]
+    if not patch_project:
+        deploy_funcs = [deploy_db,mkvirtualenv,pip_install_requirements] + deploy_funcs
+    for func in deploy_funcs: func()
+
+
+def setupnode(rollback=False, overwrite=False):
+    """
+    Install a baseline host. Can be run multiple times
+    
+    rollback=True to teardown the installation
+    This is just used to prevent having to rollback a virtual machine
+    """
+    
+    #either fabric or manage.py will setup the roles & hosts env
+    #setup_environ handles passing all the project settings into env
+    #and any woven.global_settings not already defined.
+    if not rollback:
+        port_changed = change_ssh_port()
+        #We need to assume that if the port does not get changed that
+        #it has already been done and thus we do not need to disable root
+        if port_changed:
+            disable_root()
+        upload_ssh_key()
+        restrict_ssh()
+        uncomment_sources()
+        upgrade_ubuntu()
+        setup_ufw()
+        install_packages(overwrite=overwrite)
+        set_timezone()
+
+        
+    else:
+        #rollback in reverse order of installation
+        #The only things we don't rollback are the updates
+        set_timezone(rollback)
+        install_packages(rollback)
+        setup_ufw(rollback)
+        uncomment_sources(rollback)
+        restrict_ssh(rollback)
+        upload_ssh_key(rollback)
+        disable_root(rollback)
+        change_ssh_port(rollback)
+
