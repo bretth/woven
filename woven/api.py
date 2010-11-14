40e01d6fa57a9b124502457aef2b44d29b9b1987
Brett Haydon <brett@brett-mk3.local>
2010-11-15 09:31:26 +1100

    removed rollback from api

--- woven/api.py
+++ woven/api.py
@@ -6,7 +6,6 @@ from woven.deployment import deploy_files, mkdirs
 from woven.deployment import upload_template, run_once_per_host_version
 
 from woven.environment import deployment_root, set_env, patch_project, server_state, set_server_state
-from woven.environment import project_name, project_fullname, project_version
 
 from woven.project import deploy_static, deploy_public, deploy_project, deploy_db, deploy_templates
 
@@ -31,44 +30,26 @@ def deploy():
     for func in deploy_funcs: func()
 
 
-def setupnode(rollback=False, overwrite=False):
+def setupnode(overwrite=False):
     """
     Install a baseline host. Can be run multiple times
-    
-    rollback=True to teardown the installation
-    This is just used to prevent having to rollback a virtual machine
+
     """
     
     #either fabric or manage.py will setup the roles & hosts env
     #setup_environ handles passing all the project settings into env
     #and any woven.global_settings not already defined.
-    if not rollback:
-        port_changed = change_ssh_port()
-        #We need to assume that if the port does not get changed that
-        #it has already been done and thus we do not need to disable root
-        if port_changed:
-            disable_root()
-        upload_ssh_key()
-        restrict_ssh()
-        uncomment_sources()
-        upgrade_ubuntu()
-        setup_ufw()
-        install_packages(overwrite=overwrite)
-        upload_etc()
-        set_timezone()
-        stop_webservers()
-        start_webservers()
-
-        
-    else:
-        #rollback in reverse order of installation
-        #The only things we don't rollback are the updates
-        set_timezone(rollback)
-        install_packages(rollback)
-        setup_ufw(rollback)
-        uncomment_sources(rollback)
-        restrict_ssh(rollback)
-        upload_ssh_key(rollback)
-        disable_root(rollback)
-        change_ssh_port(rollback)
+    disable_root()
+    port_changed = change_ssh_port()
+      
+    upload_ssh_key()
+    restrict_ssh()
+    uncomment_sources()
+    upgrade_ubuntu()
+    setup_ufw()
+    install_packages(overwrite=overwrite)
+    upload_etc()
+    set_timezone()
+    stop_webservers()
+    start_webservers()
 
