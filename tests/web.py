b4b85f382701b7a43c6991a93ba244da61b7d823
Brett Haydon <brett@haydon.id.au>
2010-12-01 00:14:01 +1100

    add web tests

---
+++ tests/web.py
@@ -0,0 +1,15 @@
+from fabric.api import *
+
+from woven.webservers import _site_users
+from woven.ubuntu import add_user
+
+def test_web_site_users():
+    with settings(host_string='root@192.168.188.10:22', user='root',password='root'):
+        sudo('userdel site_1')
+        users = _site_users()
+        assert not users
+        #now add a user
+        add_user(username='site_1',group='www-data',site_user=True)
+        users = _site_users()
+        assert users[0] == 'site_1'
+        
