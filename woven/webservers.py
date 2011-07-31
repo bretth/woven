8cac5a7ed054cc8063e04e6b9aedca1814907b06
Brett Haydon <brett@haydon.id.au>
2011-07-31 23:19:51 +1000

    fix issue with missing directory in Ubuntu 11.04

--- woven/webservers.py
+++ woven/webservers.py
@@ -197,7 +197,9 @@ def deploy_webconf():
             deployed += _deploy_webconf('/etc/nginx/sites-available','nginx-template.txt')
         elif 'gunicorn' in get_packages():
             deployed += _deploy_webconf('/etc/nginx/sites-available','nginx-gunicorn-template.txt')
-            
+        
+        if not exists('/var/www/nginx-default'):
+            sudo('mkdir /var/www/nginx-default')
         upload_template('woven/maintenance.html','/var/www/nginx-default/maintenance.html',use_sudo=True)
         sudo('chmod ugo+r /var/www/nginx-default/maintenance.html')
     else:
